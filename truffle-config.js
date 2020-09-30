56ce292096f29b3e2935779a65c357d0cb847811
Leonard Kimmling <58225036+l-m-s@users.noreply.github.com>
2020-09-30 11:50:53 +0200

    set compiler version, to make sure the contracts are allways compiling right

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,14 @@
 module.exports = {
-     networks: {
-          ganache: {
-               host: "localhost",
-               port: 7545,
-               network_id: "*" // Match any network id
-          }
-     }
+  networks: {
+    ganache: {
+      host: "localhost",
+      port: 7545,
+      network_id: "*", // Match any network id
+    },
+  },
+  compilers: {
+    solc: {
+      version: "0.5.16", // this the Default Truffle compiler which should be used
+    },
+  },
 };
